6b15048ea55e56c2bfda3632b37e091f21ff2c13
Cedric <cedricsegurafr@gmail.com>
2021-03-02 22:20:24 +0100

    Start navbar

--- myapp_b/src/App.js
+++ myapp_b/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import React from "react";
 import './App.css';
+import Navbar from './component_js/Navnar';
+import Layout from './component_js/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar/>
+      <Layout>
+          <h1>Hello, this will be my App</h1>
+      </Layout>
+    
     </div>
   );
 }
---
+++ myapp_b/src/component_js/Navnar.js
@@ -0,0 +1,13 @@
+import "../component_css/Navbar.css"
+
+function Navbar() {
+    return (
+      <div className="navbar">
+        <h1>This is the navbar</h1>
+      </div>
+    );
+  }
+  
+  export default Navbar;
+
+  
