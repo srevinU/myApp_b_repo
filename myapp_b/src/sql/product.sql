be3d50f9f5f418e06c3309fd16b3a48d54be5f64
Cedric <cedricsegurafr@gmail.com>
2021-03-25 20:18:54 +0100

    sql

--- myapp_b/src/sql/product.sql
+++ myapp_b/src/sql/product.sql
@@ -10,3 +10,24 @@ ALTER TABLE products
 	ADD COLUMN u_image_url VARCHAR(255),
 	ADD COLUMN u_description VARCHAR(250),
 	ADD COLUMN u_stars INT;
+
+
+
+
+INSERT INTO products (u_type, u_name, u_price, u_image_url, u_description, u_stars)
+VALUES ('Decoration',
+		'Fleur',
+		100.00,
+		'/img/galina-n-miziNqvJx5M-unsplash.jpg',
+		'Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+		2), ('Decoration',
+		'Terre',
+		95.00,
+		'/img/galina-n-miziNqvJx5M-unsplash.jpg',
+		'Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+		2), ('Decoration',
+		'Terre',
+		50.00,
+		'/img/galina-n-miziNqvJx5M-unsplash.jpg',
+		'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested',
+		5);
