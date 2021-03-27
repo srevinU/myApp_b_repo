f11267c20a1bb6ada2e86e959e6052f96df55078
Cedric <cedricsegurafr@gmail.com>
2021-03-27 21:38:03 +0100

    API product

--- myapp_b/src/services/api/product.js
+++ myapp_b/src/services/api/product.js
@@ -1,21 +1,32 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const pool = require("../utils/db.js");
 const queries = require("../utils/productQueries.js");
 const product = express();
-const port = 5432;
+
+// Request Body
+product.use(express.json())
+
+product.post("/todo", async (request, response) => {
+  let result;
+  
+  try {
+
+    let param = request.body;
+    result = await pool.query(queries.INSERT, param)
+  
+    response.json(result);
+    
+    return response.status(200).json(result.rows);
+
+  } catch (err) {
+
+    console.error(err.message);
+
+  }
+})
 
 
-product.use(bodyParser.json())
-product.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
 
-
-
-
-
-
-
-console.log(queries.SELECT_ALL)
+product.listen(pool.port, () => {
+  console.log(`App running on port ${pool.port}.`)
+})
