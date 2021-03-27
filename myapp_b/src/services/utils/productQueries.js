cd9664214b77b7ea9e06936df6296bd8ce26f62d
Cedric <cedricsegurafr@gmail.com>
2021-03-27 21:38:48 +0100

    api product

--- myapp_b/src/services/utils/productQueries.js
+++ myapp_b/src/services/utils/productQueries.js
@@ -1,7 +1,7 @@
 const queries = {
     SELECT_ALL: 'SELECT * FROM ??.??;',
-    INSERT: 'INSERT INTO ??.?? (filed, field ...) '+
-            'VALUES (?, ?, ?, ?, ?, ?, ?, ?);',
+    INSERT: 'INSERT INTO products (u_type, u_name, u_price, u_description, u_stars) '+
+            'VALUES ($1, $2, $3, $4, $5);',
     UPDATE: 'UPDATE ??.?? '+
             'SET field=?, field=?, field=?, field=?,  field=?, field=? '+
             'WHERE field=? AND field=?;',
