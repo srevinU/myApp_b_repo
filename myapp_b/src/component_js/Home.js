0ddbcc8ded30fda0708123121ee16ef9a063d909
Cedric <cedricsegurafr@gmail.com>
2021-04-06 19:56:10 +0200

    get product api

--- myapp_b/src/component_js/Home.js
+++ myapp_b/src/component_js/Home.js
@@ -1,87 +1,69 @@
-import React, { useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import "../component_css/Home.css";
-const jsonProductTest = require("../component_json/product_sample.json");
-
-
+const jsonProduct = require("../component_json/product_sample.json");
 
 function Home() {
 
-  useEffect(() => {
-    console.log(getProduct());
-  }, []);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {getProduct();}, []);
 
+  function getProduct() {
+    fetch('http://localhost:3001')
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        setProducts(data);
+      });}  
 
-function getProduct() {
-  fetch('http://localhost:3001')
-    .then(response => {
-      console.log(response);
-      return response.text();
-    })
-    .then(data => {
-      console.log(data)
-      // setMerchants(data);
-    });}
-  
+    let newContent = [];
+    let finalContent = [];
 
-
-
-
-
-
-
-  let newContent = [];
-  let finalContent = [];
-
-  jsonProductTest.forEach((element, index) => {
-    if (index !== 0 && index % 3 === 0) {
-        finalContent.push(newContent);
-        newContent = [element];
-    } else {
-        newContent.push(element);
-    }
-  });
-
-  finalContent.push(newContent);
-
-  const content = finalContent.map((elementRow, indexElementRow) => {
-    return <tr key={indexElementRow}>
-      {
-        elementRow.map((element, indexElement) => {
-          return <td key={indexElement}>
-            <ul>
-                <li className= "product_name">{element.name}</li>
-                <li className="product_image">{element.image}
-                <img className="product_image" src="/img/galina-n-miziNqvJx5M-unsplash.jpg" alt="test" id="itemImg"/>
-                </li>
-                <li className="product_description">
-                  <p>{element.description}</p>
-                  </li>
-                <li className="product_stars">
-                  {element.stars}
-                  </li>
-                <li className="product_price">{element.price}</li>
-              </ul>
-          </td>
-        })
+    products.forEach((element, index) => {
+      if (index !== 0 && index % 3 === 0) {
+          finalContent.push(newContent);
+          newContent = [element];
+      } else {
+          newContent.push(element);
       }
-
-    </tr>
-    
-  })
-
-  return (
-   
-    <div className="home">
-      <table>
-        <tbody>{content}</tbody>
-      </table>
-  </div>
-         
-       
-    
-
-  );
-}
+    });
   
-  export default Home;
+    finalContent.push(newContent);
+
+    const content = finalContent.map((elementRow, indexElementRow) => {
+      return <tr key={indexElementRow}>
+        {
+          elementRow.map((element, indexElement) => {
+            return <td key={indexElement}>
+              <ul>
+                  <li className= "product_name">{element.u_name}</li>
+                  <li className="product_image">{element.u_image}
+                  <img className="product_image" src="/img/galina-n-miziNqvJx5M-unsplash.jpg" alt="test" id="itemImg"/>
+                  </li>
+                  <li className="product_description">
+                    <p>{element.u_description}</p>
+                    </li>
+                  <li className="product_stars">
+                    {element.u_stars}
+                    </li>
+                  <li className="product_price">{element.u_price + "€"}</li>
+                </ul>
+            </td>
+          })
+        }
+      </tr>
+    })
+
+    return (
+    
+      <div className="home">
+        <table>
+          <tbody>{content}</tbody>
+        </table>
+    </div>
+          
+    );
+  }
+  
+export default Home;
--- myapp_b/src/services/utils/productQueries.js
+++ myapp_b/src/services/utils/productQueries.js
@@ -9,4 +9,6 @@ const queries = {
 }
 
 
+
+
 module.exports = queries;
