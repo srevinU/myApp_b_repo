b0d9c6affb2d9139442d56ad2f31aafd3ec3b175
Cedric <cedricsegurafr@gmail.com>
2021-04-23 21:13:16 +0200

    Css modifications

--- myapp_b/src/component_js/Management.js
+++ myapp_b/src/component_js/Management.js
@@ -11,12 +11,17 @@ export default function management() {
             <div className="sections_management">
                 
                 <div className="section section_products">
-
+                    <h1>Product</h1>
                 </div>
 
                 <div className="section section_clients">
-
+                    <h1>Client</h1>
                 </div>
+
+                <div className="section section_clients">
+                    <h1>Dashboard</h1>
+                </div>
+
             </div>
         </div>
        
