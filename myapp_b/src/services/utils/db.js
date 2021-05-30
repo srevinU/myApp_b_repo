80094a22f06f8bd30a68f45a6be700b47f310674
Cedric <cedricsegurafr@gmail.com>
2021-05-30 23:23:59 +0200

    update db

--- myapp_b/src/services/utils/db.js
+++ myapp_b/src/services/utils/db.js
@@ -1,13 +1,5 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-  user: 'cedricsegura',
-  host: 'localhost',
-  database: 'myApp_b_db',
-  password: 'Akpg*',
-  port: 5432
-})
-
 // const pool = new Pool({
 //   user: process.env.REACT_APP_DB_USER,
 //   host: process.env.REACT_APP_DB_HOST,
