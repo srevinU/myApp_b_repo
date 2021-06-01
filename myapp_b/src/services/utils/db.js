1cd2f8e51bb2050e6ef723baf0d20ba4f8681dbf
Cedric <cedricsegurafr@gmail.com>
2021-06-01 21:34:17 +0200

    db variable env set

--- myapp_b/src/services/utils/db.js
+++ myapp_b/src/services/utils/db.js
@@ -1,19 +1,12 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
-// const pool = new Pool({
-//   user: process.env.REACT_APP_DB_USER,
-//   host: process.env.REACT_APP_DB_HOST,
-//   database: process.env.REACT_APP_DB_NAME,
-//   password: process.env.REACT_APP_DB_PSW,
-//   port: process.env.REACT_APP_DB_PORT
-// })
-
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'myApp_b_db',
-//   password: 'Elephant1', 
-//   port: 5432
-// })
+const pool = new Pool({
+  user: process.env.REACT_APP_DB_USER,
+  host: process.env.REACT_APP_DB_HOST,
+  database: process.env.REACT_APP_DB_NAME,
+  password: process.env.REACT_APP_DB_PSW,
+  port: process.env.REACT_APP_DB_PORT
+})
 
 module.exports = pool;
